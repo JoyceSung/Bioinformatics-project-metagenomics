8aa616b6a09af5acb061f74b767a62aa1dff9707
Chia-Yu Sung <96060503+JoyceSung@users.noreply.github.com>
2025-04-15 16:29:55 -0500

    Update Pipeline.sh

--- Pipeline/Pipeline.sh
+++ Pipeline/Pipeline.sh
@@ -1,18 +1,14 @@
-
-# Step0. QC and multiQC check samples
+# Step0. QC and multiQC check samples' qualities
 BASE=/NFSShare/Narwhal/metagenomic
-
-mkdir -p $BASE/QC_fastqc/US
-mkdir -p $BASE/QC_fastqc/China
 for region in China ; do
   for fq in $BASE/Shotgun/$region/*.fastq.gz; do
     fastqc $fq -o $BASE/QC_fastqc/$region --threads 4
   done
 done
 
-cd /NFSShare/Narwhal/metagenomic/QC_fastqc/China
-multiqc /NFSShare/Narwhal/metagenomic/QC_fastqc/China -o /NFSShare/Narwhal/metagenomic/QC_fastqc/China/multiqc_report
-
+for region in China ; do
+  multiqc $BASE/QC_fastqc/* -o $BASE/QC_fastqc/*
+done
 
 # Step1. Install Kraken2: Kraken2 is the core tool used to classify the metagenomic reads by comparing them to a reference database of known microbial genomes.
 cd /NFSShare/Narwhal/metagenomic/tool
@@ -32,8 +28,6 @@ tar -xvzf k2_standard_08gb_202305.tgz
 
 # Step 3. Run Kraken2 on each sample (single-end R1 reads): This runs Kraken2 on your actual samples, classifying each read into a taxonomic group.
 cd /NFSShare/Narwhal/metagenomic/Shotgun
-
-BASE=/NFSShare/Narwhal/metagenomic
 mkdir -p ../kraken2_results
 for region in China US; do
 	for fq in $BASE/Shotgun/$region/*.fastq.gz; do
@@ -47,32 +41,3 @@ for region in China US; do
             --single $fq
 	done
 done
-
-# Convert all Kraken2 output files for Krona
-for file in /NFSShare/Narwhal/metagenomic/kraken2_results/*.f_output.txt; do
-    sample=$(basename "$file" .f_output.txt)
-    
-    awk '$1=="C"{print $2"\t"$3}' "$file" > /NFSShare/Narwhal/metagenomic/kraken2_results/${sample}.krona.input
-done
-
-for file in /NFSShare/Narwhal/metagenomic/kraken2_results/*.f_output.txt; do
-	sample=$(basename "$file" .f_output.txt)
-	awk '$1 == "C" && match($0, /\(taxid ([0-9]+)\)/, a) { print $2 "\t" a[1] }' "$file" > /NFSShare/Narwhal/metagenomic/kraken2_results/${sample}.krona.input
-done
-
-# Step5. Krona visualization (pretty interactive charts): Krona creates beautiful, interactive HTML plots showing taxonomic composition in a circle diagram that you can click to explore.
-cd /NFSShare/Narwhal/metagenomic/tool
-git clone https://github.com/marbl/Krona.git
-cd Krona/KronaTools
-./install.pl --prefix /NFSShare/Narwhal/metagenomic/tool/krona
-export PATH=/NFSShare/Narwhal/metagenomic/tool/krona/bin:$PATH
-
-cd /NFSShare/Narwhal/metagenomic/kraken2_results
-
-for input in /NFSShare/Narwhal/metagenomic/kraken2_results/*.krona.input; do
-    sample=$(basename "$input" .krona.input)
-
-    ktImportTaxonomy "$input" -o /NFSShare/Narwhal/metagenomic/kraken2_results/${sample}.krona.html
-done
-
-
